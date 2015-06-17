ea8d8d9958a452f5ffe0876b1042815f7cb4dfe2
Shreyak Upadhyay <shreyakupadhyay07@gmail.com>
2015-06-17 21:25:01 +0530

    updated final-script.py

--- final-script.py
+++ final-script.py
@@ -27,12 +27,29 @@ if(arr[1]=='c'):
 		options.file=raw_input("Give the name of executable file: ")
 		call(["gcc" ,sys.argv[1], "-o",options.file])
 		call(["./"+options.file])
-		os.remove(options.file)
+		
 
 	else:
 		call(["gcc" ,sys.argv[1], "-o",sys.argv[3]])
 		call(["./"+sys.argv[3]])
 		os.remove(sys.argv[3])
+
+
+elif(arr[1]=='c' and arr[2]=='p' and arr[3]=='p'):
+	parser = optparse.OptionParser()
+	parser.add_option('-n', '--executable', dest='file', help='file name')
+	(options, args) = parser.parse_args()
+
+	if options.file is None:
+		options.file=raw_input("Give the name of executable file: ")
+		call(["g++" ,sys.argv[1], "-o",options.file])
+		call(["./"+options.file])
+
+	else:
+		call(["g++" ,sys.argv[1], "-o",sys.argv[3]])
+		call(["./"+sys.argv[3]])
+		os.remove(sys.argv[3])
+
 
 
 elif(arr[1]=='p' and arr[2]=='y'):
