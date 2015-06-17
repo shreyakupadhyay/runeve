5794dda1f0f3602bead879935046c9b1d23c750f
Shreyak Upadhyay <shreyakupadhyay07@gmail.com>
2015-06-17 15:33:40 +0530

    updated final-script.py

--- final-script.py
+++ final-script.py
@@ -1,57 +1,39 @@
 import sys
 import os
-shreyak = os.path.isfile(sys.argv[2])
-if (shreyak == False):
-	filename = raw_input("Write the last name here again: ")
-	target = open (filename, 'a')
-	line1 = sys.argv[1]
-	target.write(line1)
-	print "we have added the "+ sys.argv[1] +" name to the file " + filename
-	target.close()
-else:
-	print sys.argv[2]+ " this file already exists"
+from subprocess import call
 
-file = open(sys.argv[2],"rw+")
-file.seek(0)
-count=0
+array = []
+update=0
+array= sys.argv[1]
+for j in range(0,len(array)):
+	if(array[j] == '.'):
+		update=j
+
+u=0
 arr = []
-j = 0
-for i in file.read():
-	if (i == '.'):
-		count=1
-	if (count ==1):
-		arr.append(i)
-		j=j+1;
-for k in range(0,len(arr)):
-	print arr[k]
-file.close()
+
+arr = array[update:len(array)+1]
+
+
+
+
 if(arr[1]=='c'):
 	exe=raw_input("Give the name of executable file: ")
-	import sys
-	from subprocess import call
 	call(["gcc" ,sys.argv[1], "-o",exe])
 	call(["./"+exe])
-
+	os.remove(exe)
 
 elif(arr[1]=='p' and arr[2]=='y'):
-	import sys
-	from subprocess import call
 	call(["python",sys.argv[1]])
 
-elif(arr[1]=='p' and arr[2]=='h' and arr[3]=='p')
-	import sys
-	from subprocess import call
+elif(arr[1]=='p' and arr[2]=='h' and arr[3]=='p'):
 	call(["php",sys.argv[1]])
 
 
-elif(arr[1]=='j' and arr[2]=='s')
-	import sys
-	from subprocess import call
+elif(arr[1]=='j' and arr[2]=='s'):
 	call(["node",sys.argv[1]])
 
 
-elif(arr[1]=='r' and arr[2]=='b')
-	import sys
-	from subprocess import call
+elif(arr[1]=='r' and arr[2]=='b'):
 	call(["ruby",sys.argv[1]])	
 	
